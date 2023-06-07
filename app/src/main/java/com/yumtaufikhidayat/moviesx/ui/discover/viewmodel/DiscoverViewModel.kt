0f0d50022f3fcb19b68ea371c322407868dab369
yumtaufikhidayat <yumtaufikhidayat@gmail.com>
2023-06-08 06:07:27 +0700

    Implement discover movie feature.

--- app/src/main/java/com/yumtaufikhidayat/moviesx/ui/discover/viewmodel/DiscoverViewModel.kt
+++ app/src/main/java/com/yumtaufikhidayat/moviesx/ui/discover/viewmodel/DiscoverViewModel.kt
@@ -1,4 +1,4 @@
-package com.yumtaufikhidayat.moviesx.ui.search.viewmodel
+package com.yumtaufikhidayat.moviesx.ui.discover.viewmodel
 
 import androidx.lifecycle.ViewModel
 import com.yumtaufikhidayat.moviesx.data.repository.MoviesXRepository
@@ -6,7 +6,10 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class SearchViewModel @Inject constructor(
+class DiscoverViewModel @Inject constructor(
     private val repository: MoviesXRepository
 ): ViewModel() {
+
+    fun getGenres() = repository.getGenres()
+    fun discoverMovie(query: String) = repository.discoverMovie(query)
 }
