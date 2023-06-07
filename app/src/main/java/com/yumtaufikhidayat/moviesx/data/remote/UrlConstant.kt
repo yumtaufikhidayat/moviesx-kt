9369dc0fac8962874fe13849c38c7f1b71624517
yumtaufikhidayat <yumtaufikhidayat@gmail.com>
2023-06-08 06:10:20 +0700

    Add discover movie endpoint and query.

--- app/src/main/java/com/yumtaufikhidayat/moviesx/data/remote/UrlConstant.kt
+++ app/src/main/java/com/yumtaufikhidayat/moviesx/data/remote/UrlConstant.kt
@@ -8,6 +8,8 @@ object UrlConstant {
     const val MOVIE_VIDEO = "movie/{movie_id}/videos"
     const val MOVIE_CAST = "movie/{movie_id}/credits"
     const val MOVIE_REVIEWS = "movie/{movie_id}/reviews"
+    const val DISCOVER_MOVIES = "search/movie"
     const val QUERY_PAGE = "page"
     const val QUERY_MOVIE_ID = "movie_id"
+    const val QUERY_Q = "query"
 }
