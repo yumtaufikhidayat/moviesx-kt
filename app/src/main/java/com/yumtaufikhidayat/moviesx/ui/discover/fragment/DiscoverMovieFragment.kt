58dc41653e71241cc1764c3f6eff666a464ed0b5
yumtaufikhidayat <yumtaufikhidayat@gmail.com>
2023-07-07 19:19:32 +0700

    Simplify search edittext addTextChangedListener method.

--- app/src/main/java/com/yumtaufikhidayat/moviesx/ui/discover/fragment/DiscoverMovieFragment.kt
+++ app/src/main/java/com/yumtaufikhidayat/moviesx/ui/discover/fragment/DiscoverMovieFragment.kt
@@ -3,13 +3,13 @@ package com.yumtaufikhidayat.moviesx.ui.discover.fragment
 import android.content.Context
 import android.os.Bundle
 import android.text.Editable
-import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
 import android.widget.TextView
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -89,24 +89,8 @@ class DiscoverMovieFragment : Fragment() {
                 false
             })
 
-            addTextChangedListener(object : TextWatcher {
-                override fun beforeTextChanged(
-                    s: CharSequence?,
-                    start: Int,
-                    count: Int,
-                    after: Int
-                ) {}
-
-                override fun onTextChanged(
-                    s: CharSequence?,
-                    start: Int,
-                    before: Int,
-                    count: Int
-                ) {}
-
-                override fun afterTextChanged(s: Editable?) {
-                    showDiscoverData(s)
-                }
+            addTextChangedListener(afterTextChanged = {
+                showDiscoverData(it)
             })
         }
     }
